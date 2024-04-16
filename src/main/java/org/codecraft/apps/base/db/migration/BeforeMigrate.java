c19db2b9f7c18aa998f4e69587462f251db085f0
Christophe <christophe.pauliac@gmail.com>
2024-04-16 23:10:29 +0200

    Add tomcat stratup flyway migration

---
+++ src/main/java/org/codecraft/apps/base/db/migration/BeforeMigrate.java
@@ -0,0 +1,34 @@
+package org.codecraft.apps.base.db.migration;
+
+import org.flywaydb.core.api.callback.Callback;
+import org.flywaydb.core.api.callback.Context;
+import org.flywaydb.core.api.callback.Event;
+
+public class BeforeMigrate implements Callback {
+
+  @Override
+  public boolean supports(Event event, Context context) {
+    return event == Event.AFTER_MIGRATE;
+  }
+
+  @Override
+  public boolean canHandleInTransaction(Event event, Context context) {
+    return true;
+  }
+
+  @Override
+  public void handle(Event event, Context context) {
+    if (event == Event.AFTER_MIGRATE) {
+      refreshMetaData();
+    }
+  }
+
+  @Override
+  public String getCallbackName() {
+    return "Refresh meta data";
+  }
+
+  private void refreshMetaData() {
+    System.out.println("Refresh meta data");
+  }
+}
